692b815efeb2a29c8f4419fc818c8fce6060d34c
srikanth <srikanthrdy7979@gmail.com>
2024-12-10 09:59:06 +0530

    backend file

--- project1/EKS-TF/backend.tf
+++ project1/EKS-TF/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "srikanthyoutubeclone" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-east-1"
+    region = "ap-south-1"
   }
 }
